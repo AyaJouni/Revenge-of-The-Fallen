4213ddfe64f481a08b502617cfed5da3f2e43b8a
farahalnassar <farahalnassar21@gmail.com>
2025-03-19 11:54:44 +0200

    Latest updates

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 # run.py
 from server import app
 
 if __name__ == '__main__':
+=======
+# run.py
+from server import app
+
+if __name__ == '__main__':
+>>>>>>> 5fcb68a (Pushing latest updates)
     app.run(debug=True, host='0.0.0.0', port=5000)
